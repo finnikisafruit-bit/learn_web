17d2d7669dbef54bc94b57a0fe4756a140fc752c
Dmitriy Zaikin <finnikisafruit@gmail.com>
2026-06-14 21:19:16 +0300

    Добавили поддержку базы данных

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
+
+from webapp.model import db, News
+
 
 def get_html(url):
     try:
@@ -17,15 +22,22 @@ def get_python_news():
         all_news = soup.find('ul', class_='list-recent-posts').findAll('li')
         result_news = []
         for news in all_news:
+            link = news.find('a')
             title = news.find('a').text
-            url = news.find('a').href
+            url = link['href']
             published = news.find('time').text
-            result_news.append({
-                "title": title,
-                "url": url,
-                "published": published          
-            })
-        return result_news
-    return False
+            try:
+                published = datetime.strptime(published, '%Y-%m-%d')
+            except ValueError:
+                published = datetime.now()
+            save_news(title, url, published)
 
 
+def save_news(title, url, published):
+
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)
+    if not news_exists:
+        news_news = News(title=title, url=url, published=published)
+        db.session.add(news_news)
+        db.session.commit()
